67d32070ba9b3056b1fb06ac8b4b20197ecd4e6e
Tatsuya Suzuki <tatsuya.suzuki@sage.com>
2024-02-23 14:43:20 +0000

    Define spec for VERSION

---
+++ spec/simplecov/formatter/version_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Simplecov::Formatter::StdoutLinePrinter do
+  it do
+    expect(
+      described_class::VERSION
+    ).to match(
+      /^([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?$/
+    )
+  end
+
+end
